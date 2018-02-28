981cd999a726a3a74ef7db52be9d6b3467076531
Sunconure11 <eyespot7b@aol.com>
2018-02-27 22:52:40 -0500

    you know these burgers are obviously foreskin

--- src/main/java/com/Sunconure11/DragonMounts/DragonMounts.java
+++ src/main/java/com/Sunconure11/DragonMounts/DragonMounts.java
@@ -37,8 +37,8 @@ public class DragonMounts {
 	public static final String VERSION = "1.0.2 Alpha";
 
 	@SidedProxy(
-			serverSide = "CommonProxy",
-			clientSide = "ClientProxy"
+			serverSide = "com.Sunconure11.DragonMounts.server.CommonProxy",
+			clientSide = "com.Sunconure11.DragonMounts.client.ClientProxy"
 	)
 	public static CommonProxy proxy;
 
